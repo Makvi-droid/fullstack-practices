f05b3934c2ca6fefec6001f820b5d9ae3f38771a
Makvi-droid <pasbenauro05@gmail.com>
2025-07-17 03:37:49 +0800

    initial commit

---
+++ mern/workOutSessions/backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+
+//express app
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to my shit"})
+})
+
+//listen for requests 
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
+
